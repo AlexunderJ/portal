90c2747cfa89927254ffd8d31138df62ffa7dcc8
AlexunderJ <pozdrower@wp.pl>
2020-11-03 20:18:19 +0100

    admin form for add news

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.db, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(' were connected db!');
+});
+
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+
 
 
 var app = express();
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
 
 router.all('*',(req,res,next)=>{
@@ -14,9 +15,27 @@ router.all('*',(req,res,next)=>{
 router.get('/', (req, res) =>{
 
   
-  res.render('admin', { title: 'Admin' });
+  res.render('admin/index', { title: 'Admin' });
 });
 
+router.get('/news/add', (req,res)=>{
+  res.render('admin/news-form', { title: 'Dodaj Newsa', body: {}, errors:{} });
+})
 
+router.post('/news/add', (req,res)=>{
+  const body = req.body;
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  newsData.save((err)=>{
+    if(err){
+      res.render('admin/news-form', {title: 'Dodaj Newsa', errors, body});
+      return;
+    }
+
+    res.redirect('/admin')
+  
+  });
+});
 
 module.exports = router;
