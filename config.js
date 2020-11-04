6b38da897a513c98e696a75c62ba5218d2e62199
AlexunderJ <pozdrower@wp.pl>
2020-11-04 22:27:18 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin_olek:DW0VtiudDurYNuW7@cluster0.nm5gn.mongodb.net/<dbname>?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession:24 * 60 * 60 *1000
 }
